95397ce323d02800c93dfd29cb4e1f53b5dcfd5a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-05 12:20:50 -0300

    forms

---
+++ exemploformas/oi.js
@@ -0,0 +1,50 @@
+/*
+
+console.log("OLA\nMUNDO")
+
+console.log("Nome\tNota\tTurma")
+
+console.log("Pedro\t10\t2A")
+
+console.log("Maria\t8\t2B")
+
+console.log("Anastacio\t8,5\t2C")
+
+
+
+
+let nome = "Pedro"
+
+console.log("Oi meu amigo"+nome.toUpperCase())
+
+
+
+
+let valor1 = 10
+
+let valor2 = 20
+
+let valor3 = valor1 + valor2
+
+console.log("A soma dos dois valores é "(valor1+valor2))
+
+*/
+
+
+
+
+const leia = require("prompt-sync")()
+
+let numero1, numero2 = 0
+
+
+
+
+numero1 = leia("Digite o primeiro numero: ")
+
+numero2 = leia("Digite o segundo numero: ")
+
+
+
+
+console.log("A soma desses números é"+numero1+"mais"+numero2+"é"+(numero1+numero2))
