cfd70497956d7b959f3095253fa13ec0387e4369
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-13 13:38:58 -0300

    RevPoo

---
+++ revpoo/boletim.js
@@ -0,0 +1,34 @@
+const leia = require("prompt-sync")()
+
+
+
+
+//arrays e vetores
+
+let alunos = ['Raul','Duarte','Digolinha','Vitin']
+
+let notas = [0,0,0,0]
+
+
+
+
+for(let x=0;x<alunos.length;x++){
+
+    console.log(alunos[i])
+
+    notas[x] = parseInt(leia("Digite quantos notas esse aluno tem: "))
+
+}
+
+
+
+
+console.log("Boletim")
+
+console.log("aluno\tnotas")
+
+for(let x=0;x<=aluno.length;x++){
+
+    console.log(aluno[x]+'\t'+notas[x])
+
+}
