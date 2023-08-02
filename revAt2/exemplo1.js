3f6a4040bc8bff595a12777c6b111409397171bd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-02 09:58:01 -0300

    At2

---
+++ revAt2/exemplo1.js
@@ -0,0 +1,37 @@
+const leia= require("prompt-sync")()
+
+let nome = {}
+
+nomes[0] = "Luciano"
+nomes[1] = 16
+nomes[10] = {
+    nome : "Luciano",
+    idade : 16
+}
+
+console.log(nomes)
+console.log(nomes[4])
+console.log(nomes[10])
+console.log(nomes.length)
+
+let mat =[]
+
+mat[0] = "mat0"
+mat[1] = "mat1"
+mat[2] = "ma2"
+
+let nomes = ['Lucas','Pedro','Marcos']
+
+let notas = []
+
+notas[0] = "0"
+notas[1] = "0"
+notas[2] = "0"
+
+let notas = [5,6,7]
+console.log("Mat\Nomes\Notas")
+for(let x=0; x=nome.length; x++){
+    if(notas[x<=5]){
+        situacao = 'Rec'
+    }
+}
