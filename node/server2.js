59f31194e7a6618155269727196745e94ed73a15
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 08:59:01 -0300

    node

---
+++ node/server2.js
@@ -0,0 +1,15 @@
+const {createServer} = require('http')
+
+let server = createServer((request,response)=>{
+    Response.writeHead(200,{"Content-Type":"text/html"});
+    response.write(`
+    <h1> Hello World 2</h1>
+    <p>Pagina html usando node.js </p>
+    `);
+    response.end();
+})
+
+const port = 8000;
+server.listen(port,()=>{
+    console.log(`Usando a porta ${port} na url http://localhost:${port}`)
+})
